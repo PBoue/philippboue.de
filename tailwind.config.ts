1da9e5a532bbff93a892495947dcbd68095f0be5
Philipp Boue <philipp.boue@gmail.com>
2023-09-07 22:40:51 +0200

    SM: Custom Type Global Settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,22 @@ const config: Config = {
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        colors: {
+            'black': '#0D1F2D',
+            'white': '#FFFFFF',
+            'magenta': '#F63BCD',
+            'red': '#A30015',
+            'blue': '#3BBFF6',
+            'cyan': '#3BF6EB',
+            'green': '#4CB963',
+            'yellow': '#FDE74C'
+        },
         extend: {
             fontFamily: {
                 body: ['var(--font-montserrat)']
